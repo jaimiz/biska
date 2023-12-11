a6dd704d17e8e7220efc70157e182373b929e095
Jaimiz <bond@jaimiz.bond>
2023-12-11 14:05:08 +0100

    feat: add external link embeds
    also fixed the issue with profile display names
    
    Closes #12

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,8 @@ export const Variants = cva(
 					"bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
+				invisible:
+					"bg-[none] font-[inherit] color-[inherit] border-[none] p-0 cursor-pointer",
 			},
 			size: {
 				default: "h-10 px-4 py-2",
--- src/components/user/profile-display-name.tsx
+++ src/components/user/profile-display-name.tsx
@@ -1,22 +1,30 @@
 import { ProfileViewDetailed } from "@atproto/api/dist/client/types/app/bsky/actor/defs";
-import { Link } from "react-router-dom";
+import { AppLink } from "../link";
+import { ClassValue } from "clsx";
+import { cn } from "@/lib/utils";
 
 type ProfileDisplayNameProps = {
 	profile: ProfileViewDetailed;
+	className?: ClassValue;
 };
 
 export function ProfileDisplayName(props: ProfileDisplayNameProps) {
-	const { profile } = props;
+	const { profile, className } = props;
 	return (
-		<Link to={`/profile/${profile.handle}`} className="flex items-center">
-			<div className="flex gap-x-2">
-				{profile.displayName ? (
-					<div className="font-medium">{`${profile.displayName}`}</div>
-				) : null}
-				<div className="text-zinc-500 dark:text-zinc-400">
-					@{profile.handle}
-				</div>
+		<AppLink
+			to={`/profile/${profile.handle}`}
+			className={cn(
+				"flex items-center gap-x-1 whitespace-nowrap max-w-[80%] overflow-hidden",
+				className,
+			)}
+		>
+			{profile.displayName ? (
+				<div className="font-medium">{`${profile.displayName}`}</div>
+			) : null}
+			<div className="text-zinc-500 dark:text-zinc-400 text-ellipsis overflow-hidden">
+				{" "}
+				@{profile.handle}
 			</div>
-		</Link>
+		</AppLink>
 	);
 }
