e633855b4d45f411651018a63933fe3ed0b8a9cc
Jaimiz <bond@jaimiz.bond>
2023-12-14 11:17:21 +0100

    feat: remove expanded view
    Also do some profile tweaks, like the banner image, showing blocked/blocking status properly and optmize profile loading query

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,21 +3,7 @@ import { RefAttributes } from "react";
 import {
 	Link as RouterLink,
 	LinkProps as RouterLinkProps,
-	To,
 } from "react-router-dom";
-
-const EXPANDED_LINK_PREFIX = "/expand";
-export function generateExpandOrShrinkLink(location: To) {
-	const currentLocation =
-		typeof location === "string" ? location : location.pathname;
-	if (!currentLocation) {
-		return location;
-	}
-	if (currentLocation.startsWith(EXPANDED_LINK_PREFIX)) {
-		return currentLocation.replace(EXPANDED_LINK_PREFIX, "");
-	}
-	return `${EXPANDED_LINK_PREFIX}${currentLocation}`;
-}
 
 type LinkProps = RouterLinkProps & RefAttributes<HTMLAnchorElement>;
 export function AppLink(props: LinkProps) {
@@ -36,8 +22,4 @@ export function ContainerLink({ children, className, ...rest }: LinkProps) {
 			{children}
 		</div>
 	);
-}
-
-export function ExpandOrShrinkLink({ to, ...props }: LinkProps) {
-	return <RouterLink to={generateExpandOrShrinkLink(to)} {...props} />;
 }
