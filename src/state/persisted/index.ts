59de92280503aef911300c7e5e04b6aebac79955
Jaimiz <bond@jaimiz.bond>
2024-01-11 18:01:00 +0100

    chore: move persistent store to state file

--- src/state/persisted/index.ts
+++ src/state/persisted/index.ts
@@ -1,15 +1,40 @@
 import { atom, getDefaultStore } from "jotai";
+import * as localforage from "localforage";
 import { AppSchema, AppSchemaKey, defaultAppSchema } from "./schema.ts";
-import * as store from "./store.ts";
 
 const rootStateAtom = atom<AppSchema>(defaultAppSchema);
 export const stateStore = getDefaultStore();
+const BISKA_STORAGE_VERSION = "0.0.0";
+
+localforage.config({
+	name: `BISKA_v${BISKA_STORAGE_VERSION}`,
+});
+
+export const BISKA_STORAGE_KEY = "BISKA";
+
+const storage = {
+	async write(value: AppSchema) {
+		try {
+			AppSchema.parse(value);
+			await localforage.setItem(BISKA_STORAGE_KEY, value);
+		} catch (e) {
+			console.log("error writing to persistent storage");
+		}
+	},
+
+	async read(): Promise<AppSchema | undefined> {
+		const rawData = await localforage.getItem<unknown>(BISKA_STORAGE_KEY);
+		if (AppSchema.safeParse(rawData).success) {
+			return rawData as AppSchema;
+		}
+	},
+};
 
 export async function init() {
 	try {
-		const stored = await store.read(); // check for new store
+		const stored = await storage.read(); // check for new store
 		// TODO: check if app version has changed
-		if (!stored) await store.write(defaultAppSchema); // opt: init new store
+		if (!stored) await storage.write(defaultAppSchema); // opt: init new store
 		stateStore.set(rootStateAtom, stored || defaultAppSchema);
 	} catch (e) {
 		console.error("persisted state: failed to load root state from storage", {
@@ -30,7 +55,7 @@ export function write<K extends AppSchemaKey>(key: K, value: AppSchema[K]) {
 			...prevState,
 			[key]: value,
 		}));
-		store.write(stateStore.get(rootStateAtom));
+		storage.write(stateStore.get(rootStateAtom));
 	} catch (e) {
 		console.error("error persisting state to store");
 	}
